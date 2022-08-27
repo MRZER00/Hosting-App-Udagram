d82e7f3cd7fedbfee1ee323d760570a821ff3864
0X0ŽĒR∅⁰ <71743035+MRZER00@users.noreply.github.com>
2022-08-27 17:18:17 +0300

    Update deploy.sh

--- udagram-api/bin/deploy.sh
+++ udagram-api/bin/deploy.sh
@@ -1,5 +1,5 @@
 eb init udagram_fullstack --platform node.js --region us-east-1
-eb create udagramfullstack-env --region us-east-1
+# eb create udagramfullstack-env --region us-east-1
 eb use udagramfullstack-env
 eb deploy
 eb health
@@ -9,4 +9,4 @@ eb health
 
 eb setenv AWS_ACCESS_KEY_ID="$AWS_ACCESS_KEY_ID" AWS_SECRET_ACCESS_KEY="$AWS_SECRET_ACCESS_KEY" AWS_DEFAULT_REGION="$AWS_DEFAULT_REGION" POSTGRES_USERNAME="$POSTGRES_USERNAME" POSTGRES_PASSWORD="$POSTGRES_PASSWORD" POSTGRES_DB="$POSTGRES_DB" PORT="$PORT" POSTGRES_HOST="$POSTGRES_HOST" AWS_REGION="$AWS_REGION" AWS_PROFILE="$AWS_PROFILE" AWS_BUCKET="$AWS_BUCKET" URL="$URL" JWT_SECRET="$JWT_SECRET" --profile default
 
-eb open
+# eb open
