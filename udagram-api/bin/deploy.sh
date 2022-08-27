7aa44dc540f9ff7acf28dd75c05de8433cef6760
MRZER00 <www.ahmedlotfy@yahoo.com>
2022-08-27 20:31:31 +0300

    update

--- udagram-api/bin/deploy.sh
+++ udagram-api/bin/deploy.sh
@@ -1,5 +1,5 @@
-eb init Udagram_Full-Stack --platform node.js --region us-east-1
-eb use Udagramfullstack-env
+# eb init udagram_fullstack --platform node.js --region us-east-1
+eb use udagramfullstack-env
 eb deploy
 eb health
 # if you want read the logs
